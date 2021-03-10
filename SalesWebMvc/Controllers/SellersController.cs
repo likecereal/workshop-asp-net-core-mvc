ee63de0b7f3ddf4cfba3aec7012e9f13bf955b8d
Aires <aires.ribeiro.menezes@hotmail.com>
2021-03-10 15:56:01 +0000

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -39,6 +39,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var deparments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = deparments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -103,6 +110,14 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+
+            if (!ModelState.IsValid)
+            {
+                var deparments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = deparments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id missmatch" });
